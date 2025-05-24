2acb11a94506dfeaa97f594e26e7572009484cef
BidhanMainali <BidhanMainali@hotmail.com>
2025-05-23 19:01:28 -0700

    this is an updated file

--- f.java
+++ f.java
@@ -1,5 +1,16 @@
-
-
-public static void main(String[] args) {
+public class f {
+// This is a test to see if it can be pushed into repository and create pull
+    public static void main(String[] args) {
+        
     System.out.println("Hello world");
+    System.out.println("This is a new print line modified");
+
+    int a =10;
+
+    if (a == 10) {
+        System.out.println("a is 10");
+    } else {
+        System.out.println("a is not 10");
+    }
+}
 }
