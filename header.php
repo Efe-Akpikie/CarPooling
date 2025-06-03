85d97549848245e82d6c830834398d3437c3b055
BidhanMainali <BidhanMainali@hotmail.com>
2025-06-02 18:36:30 -0700

    added a backdrop to the main page

---
+++ header.php
@@ -0,0 +1,9 @@
+<!---- header.php ---->
+<link rel="stylesheet" href="styles.css">
+<header>
+    <nav class = "navbar">
+        <div class="logo">
+           <div class = "Test"><a href="index.php">Car Pooling</a></div> 
+        </div>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <title>Car Pooling</title>
+<head>
+    
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    
+
+    <!-- Background Header Section -->
+    <div class="BGheader">
+        
+        <h1>Car Pooling</h1>
+        <p>Welcome to the Car Pooling service!222</p>
+    </div>
+
+    <!-- Page Content -->
+    <div class="content">
+        <p>This is a test</p>
+    </div>
+
+</body>
+</html>
